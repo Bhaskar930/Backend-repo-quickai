8024b6b2712e576dbbd5dd762a496ecf9e88fc78
Bhaskar <negibhaskar19@gmail.com>
2025-09-01 14:10:34 +0530

    minor change

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { auth } from '../middleware/auth.js';
-import { generateArticle, generateBlogTitle, generateImage, RemoveImageBackground, RemoveImageObject, ResumeReview } from '../controllers/AiController.js';
 import { upload } from '../configs/multer.js';
+import { generateArticle, generateBlogTitle, generateImage, RemoveImageBackground, RemoveImageObject, ResumeReview } from '../controllers/AiController.js';
 
 const aiRouter=express.Router();
 
